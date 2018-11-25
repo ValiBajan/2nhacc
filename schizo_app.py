5e3bfd79a0008d5d5169209147d88f3ad02c3a31
ValiBajan <45309696+ValiBajan@users.noreply.github.com>
2018-11-25 13:28:16 +0200

    Add files via upload

--- schizo_app.py
+++ schizo_app.py
@@ -1,41 +1,35 @@
-#import sys
 import tkinter as tk
 import os
-from tkinter import *
-from tkinter import filedialog as fd
+from tkinter.filedialog import askopenfilename
 import process_nii
-#from tkinter import ttk
-#from tkinter import filedialog
-#os.chdir('C:\\2NHack')
-curDir = str(os.getcwd())
+from PIL import Image, ImageTk
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+os.chdir(dir_path)
 xCrop = [30, 122, 175, 285, 345, 440]
 yCrop = [50, 138, 50, 140, 45, 155]
 
 class Browse(tk.Frame):
-    """ Creates a frame that contains a button when clicked lets the user to select
-    a file and put its filepath into an entry.
-    """
-    
+
     def __init__(self, master, initialdir='', filetypes=()):
+        #tk.Toplevel( width=400, height=100 )
         super().__init__(master)
         self.filepath = tk.StringVar()
         self._initaldir = initialdir
         self._filetypes = filetypes
         self._create_widgets()
         self._display_widgets()
-                
+
     def _create_widgets(self):
-        back = tk.Frame( width=400, height=200).pack()
-        self._label = tk.Label(text='Ur mom gay', fg='blue')
-        self._label.pack()
         self._entry = tk.Entry(self, textvariable=self.filepath)
         self._entry.pack()
         self._button = tk.Button(self,  text="Browse", command=self.browse)
         self._button.pack()
-        self._button = tk.Button(self, text='Analyze', fg='red', command = self.chestie)
+        self._button = tk.Button(self, text='Analyze', fg='red', command = self.analyze)
         self._button.pack()
-        
-    
+        self._button = tk.Button(self, text='Results', command = self.new_window)
+        self._button.pack()
+
     def _display_widgets(self):
         self._entry.pack(fill='x', expand=True)
         #self.browse()
@@ -45,37 +39,37 @@ class Browse(tk.Frame):
         #string = os.path.abspath("test_place.py")
         #f.write(file_browser.filepath.get())
         #f.close
-        
-    def chestie(self):
-        if (os.path.isfile(niiFile)):
-            process_nii.nii2jpg(inFile=niiFile, outFile='{}brain.jpg'.format(curDir))
-            process_nii.splitAndConvert(inFile='{}brain.jpg'.format(curDir), outDir='{}'.format(curDir), fileNumber='', gray=True, xCrop=xCrop, yCrop=yCrop)
+
+    def browse(self):
+        self.filepath = askopenfilename(filetypes=(("MRI Scan", "*.nii"),
+                                             ("All files", "*.*") ))
+
+    def analyze(self):
+        #os.chdir('C:/2NHack/Dataset/')
+        #niiFile = os.path.relpath(niiFile)
+        #niiFile = os.path.relpath(niiFile)
+        print(self.filepath)
+        self.filepath = os.path.relpath(self.filepath)ss
+        if (os.path.isfile(self.filepath)):
+            process_nii.nii2jpg(inFile=self.filepath, outFile='brain.jpg')
+            process_nii.splitAndConvert(inFile='brain.jpg', fileNumber='', gray=True, xCrop=xCrop, yCrop=yCrop)
         else:
             print ('Incorrect file path')
-    
-    def browse(self):
-        self.filepath.set(fd.askopenfilename(initialdir=self._initaldir,
-                                             filetypes=self._filetypes))
-        niiFile = file_browser.filepath.get()
-        #niiFile = niiFile[18:]
-        print(niiFile)
+            
+    def new_window(self):
+        self._top = tk.Toplevel()
+        self._img = ImageTk.PhotoImage(Image.open('brain.jpg'))
+        self._imgLabel = tk.Label(self, image=self._img)
+        self._imgLabel.pack(side='bottom', fill='both', expand='yes')
+        self._resLabel = tk.Label(self, text='Probabilitate schizofrenie: 100%')
+        self._resLabel.pack(side='bottom', fill='both', expand='yes')
         
-        
-        
-    
-        
-if __name__ == '__main__':
-    #print(str(os.getcwd()))
+
+if __name__ == "__main__": 
     root = tk.Tk()
-    
-    file_browser = Browse(root, initialdir=str('{}\\'.format(curDir)),
-                                filetypes=(('NII','*.nii'),
-                                                            ("All files", "*.*")))
+    file_browser = Browse(root, initialdir=str(os.getcwd()))
     file_browser.pack(fill='x', expand=True)
-    #if (os.path.isfile(niiFile)):
-    #    process_nii.nii2jpg(inFile=niiFile, outFile='{}brain.jpg'.format(curDir))
-    #    process_nii.splitAndConvert(inFile='{}brain.jpg'.format(curDir), outDir='{}'.format(curDir), fileNumber='', gray=True, xCrop=xCrop, yCrop=yCrop)
-    #else:
-    #   print ('Incorrect file path')
+    
+    
     
     root.mainloop()
